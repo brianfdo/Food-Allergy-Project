ef73986995732fb703568968dd291834c7fd39e9
Brian Fernando <brianfdo@users.noreply.github.com>
2020-12-28 15:34:27 -0500

    changed from ListView to RecyclerView for ingredient list

--- app/src/main/java/com/example/ingredientallergyapp/InputIngredientActivity.java
+++ app/src/main/java/com/example/ingredientallergyapp/InputIngredientActivity.java
@@ -1,11 +1,14 @@
 package com.example.ingredientallergyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,20 +18,19 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class InputIngredientActivity extends AppCompatActivity {
+public class InputIngredientActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener {
 
     RecyclerView recyclerView;
     EditText ingredientInput;
     Button addIngredient, confirmInput;
 
     ArrayList<String> ingredients = new ArrayList<>();
-    ArrayAdapter arrayAdapter1;
+    RecyclerViewAdapter adapter;
 
     Integer indexValue;
     String item;
 
 
-    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,13 +41,16 @@ public class InputIngredientActivity extends AppCompatActivity {
         confirmInput = (Button) findViewById(R.id.confirmInput);
         ingredientInput = (EditText) findViewById(R.id.ingredientInput);
 
-        //setup arrayAdapter for listView
-        arrayAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ingredients);
-        recyclerView.setAdapter(arrayAdapter1);
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(this, ingredients);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
-
-        //add ingredients to listView
-        addIngredient.setOnClickListener(v -> {
+        //add ingredients to RecyclerView
+        addIngredient.setOnClickListener(v ->
+        {
             String input = ingredientInput.getText().toString();
             boolean add = true;
             if (input.equals("")) {
@@ -59,27 +64,20 @@ public class InputIngredientActivity extends AppCompatActivity {
             if (add) {
                 ingredients.add(input);
             }
-            arrayAdapter1.notifyDataSetChanged();
+            adapter.notifyDataSetChanged();
             ingredientInput.setText("");
         });
 
-        //delete ingredients from listView
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                item = parent.getItemAtPosition(position).toString() + "has been deleted";
-                Toast.makeText(InputIngredientActivity.this, item, Toast.LENGTH_SHORT).show();
-
-                ingredients.remove(position);
-                arrayAdapter1.notifyDataSetChanged();
-                
-                return true;
-            }
+        ingredientInput.setOnClickListener(v -> {
+            ingredientInput.setText("");
+            adapter.notifyDataSetChanged();
         });
-
     }
 
-
-
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
 
 }
+
