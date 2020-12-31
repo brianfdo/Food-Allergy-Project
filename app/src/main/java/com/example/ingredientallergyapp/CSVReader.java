83216be60806eda7a25a08f7eddf28a8d463fdbe
Brian Fernando <brianfdo@users.noreply.github.com>
2020-12-31 16:58:05 -0500

    finish csv extraction and create editText and search button for food input

--- app/src/main/java/com/example/ingredientallergyapp/CSVReader.java
+++ app/src/main/java/com/example/ingredientallergyapp/CSVReader.java
@@ -13,20 +13,21 @@ public class CSVReader {
     public static void addFood(BrandedFood item) {
         brandedFoods.add(item);
     }
-
-    //public static String foodName(int i) {
-    //return ;
-    //}
-
+    
     public static void readFile() {
         File f = new File("Products.csv");
         try {
             FileReader fr = new FileReader(f);
             BufferedReader bfr = new BufferedReader(fr);
+            int counter = 0;
             String line = bfr.readLine();
             while (line != null) {
+                while (counter < 2) {
+                    line = bfr.readLine();
+                    counter++;
+                }
                 int nbdNumber;
-                String foodname;
+                String foodName;
                 String manufacturer;
                 String ingredients;
 
@@ -39,16 +40,49 @@ public class CSVReader {
                 if (line.startsWith("\"")) {
                     line = line.substring(1);
                     index = line.indexOf("\"");
-                    foodname = line.substring(0, index);
-                    line = line.substring(index + 1);
+                    foodName = line.substring(0, index);
+                    line = line.substring(index + 2);
                 } else {
                     index = line.indexOf(",");
-                    foodname = line.substring(0, index);
-                    line = line.substring(index);
+                    foodName = line.substring(0, index);
+                    line = line.substring(index + 1);
                 }
 
                 //extract manufacturer
-                
+                for (int i = 0; i < 2; i++) {
+                    index = line.indexOf(",");
+                    line = line.substring(index + 1);
+                }
+                if (line.startsWith("\"")) {
+                    line = line.substring(1);
+                    index = line.indexOf("\"");
+                    manufacturer = line.substring(0, index);
+                    line = line.substring(index + 2);
+                } else {
+                    index = line.indexOf(",");
+                    manufacturer = line.substring(0, index);
+                    line = line.substring(index + 1);
+                }
+                if (manufacturer == null || manufacturer.equals("")) {
+                    manufacturer = "N/A";
+                }
+
+                //extract ingredients
+                for (int i = 0; i < 2; i++) {
+                    index = line.indexOf(",");
+                    line = line.substring(index + 1);
+                }
+                if (line.startsWith("\"")) {
+                    line = line.substring(1);
+                    index = line.indexOf("\"");
+                    ingredients = line.substring(0, index);
+                } else {
+                    ingredients = line;
+                }
+
+                //create food item object
+                BrandedFood brandedFood = new BrandedFood(nbdNumber, foodName, manufacturer, ingredients);
+                addFood(brandedFood);
 
                 line = bfr.readLine();
             }
--- app/src/main/java/com/example/ingredientallergyapp/InputFoodActivity.java
+++ app/src/main/java/com/example/ingredientallergyapp/InputFoodActivity.java
@@ -3,15 +3,25 @@ package com.example.ingredientallergyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class InputFoodActivity extends AppCompatActivity {
 
+    Button searchInput;
+    EditText ingredientInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_foodname);
+
+        searchInput = (Button) findViewById(R.id.searchInput);
+        ingredientInput = (EditText) findViewById(R.id.ingredientInput);
+
+
     }
 
     ArrayList<String> ingredients = InputIngredientActivity.getIngredients();
